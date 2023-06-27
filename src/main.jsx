023bdc18b3f4f30dd64068fd666f82c07aaa14a1
Chris Najman <christopher.najman@gmail.com>
2023-06-27 18:26:21 +0100

    feat: routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Home } from "./pages/Home.jsx";
+import { Contact } from "./pages/Contact.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/vite-basic/",
+    element: <App />,
+    children: [
+      {
+        path: "/vite-basic/",
+        element: <Home />,
+      },
+      {
+        path: "/vite-basic/contact",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
